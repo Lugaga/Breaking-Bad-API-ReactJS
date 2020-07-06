870cc155e25f3040451638482499e65f65488a79
lugaga <maureezgaga@gmail.com>
2020-07-06 12:04:05 +0300

    add search query feature

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react'
 
 const Search = () => {
+    const [text, setText] = useState ('')
+
+    const onChange = (q) => {
+        setText(q)
+        getQuery(q)
+    }
     return (
        <section className='search'>
         <form>
 
-        <input type='text' className='form-control' placeholder='Search Character'
+        <input 
+        type='text' 
+        className='form-control' 
+        placeholder='Search Character'
+        value={text}
+        onChange={(e) => onChange (e.target.value)}
         autoFocus
         />
         </form>
