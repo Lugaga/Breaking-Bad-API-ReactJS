05093a0e7720f49c85d8955aeee63c9d8d570199
lugaga <maureezgaga@gmail.com>
2020-07-06 16:56:12 +0300

    Updates

---
+++ precache-manifest.dacc7a7215c6ce369458812796508ce9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6545f531cdba09e5aae669a42e8ce28b",
+    "url": "/Breaking-Bad-API-ReactJS/index.html"
+  },
+  {
+    "revision": "2c1e363f801f3ab0e324",
+    "url": "/Breaking-Bad-API-ReactJS/static/css/main.f58d3d68.chunk.css"
+  },
+  {
+    "revision": "6052fc51229b7a6d0e90",
+    "url": "/Breaking-Bad-API-ReactJS/static/js/2.287383d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Breaking-Bad-API-ReactJS/static/js/2.287383d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c1e363f801f3ab0e324",
+    "url": "/Breaking-Bad-API-ReactJS/static/js/main.add2702b.chunk.js"
+  },
+  {
+    "revision": "f1389d068fe119289779",
+    "url": "/Breaking-Bad-API-ReactJS/static/js/runtime-main.174b133a.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad-API-ReactJS/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking-Bad-API-ReactJS/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "138eb5ed72ef5f72499ce7e404711544",
+    "url": "/Breaking-Bad-API-ReactJS/static/media/spinner.138eb5ed.gif"
+  }
+]);
